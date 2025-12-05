32d7646e3fe2d479d6f14d9823d6bb0770e85627
ehu <ehu@158.227.124.25>
2025-12-05 13:03:27 +0100

    -mas hibernate

--- src/main/java/businessLogic/BLFacadeImplementation.java
+++ src/main/java/businessLogic/BLFacadeImplementation.java
@@ -1,120 +1,62 @@
 package businessLogic;
+
 import java.util.Date;
 import java.util.List;
-import java.util.ResourceBundle;
 
-
-import configuration.ConfigXML;
-import dataAccess.DataAccess;
+import dataAccess.HibernateDataAccess;
 import domain.Ride;
-import domain.Driver;
 import exceptions.RideMustBeLaterThanTodayException;
 import exceptions.RideAlreadyExistException;
 
-/**
- * It implements the business logic as a web service.
- */
-public class BLFacadeImplementation  implements BLFacade {
-	DataAccess dbManager;
+public class BLFacadeImplementation implements BLFacade {
 
-	public BLFacadeImplementation()  {		
-		System.out.println("Creating BLFacadeImplementation instance");
-		
-		
-		    dbManager=new DataAccess();
-		    
-		//dbManager.close();
+    private HibernateDataAccess dbManager;
 
-		
-	}
-	
-    public BLFacadeImplementation(DataAccess da)  {
-		
-		System.out.println("Creating BLFacadeImplementation instance with DataAccess parameter");
-		ConfigXML c=ConfigXML.getInstance();
-		
-		dbManager=da;		
-	}
-    
-    
-    /**
-     * {@inheritDoc}
-     */
-     public List<String> getDepartCities(){
-    	dbManager.open();	
-		
-		 List<String> departLocations=dbManager.getDepartCities();		
-
-		dbManager.close();
-		
-		return departLocations;
-    	
+    public BLFacadeImplementation() {
+        System.out.println("Creating BLFacadeImplementation with HIBERNATE");
+        dbManager = new HibernateDataAccess();
     }
-    /**
-     * {@inheritDoc}
-     */
-	 public List<String> getDestinationCities(String from){
-		dbManager.open();	
-		
-		 List<String> targetCities=dbManager.getArrivalCities(from);		
 
-		dbManager.close();
-		
-		return targetCities;
-	}
+    public BLFacadeImplementation(HibernateDataAccess da) {
+        System.out.println("Creating BLFacadeImplementation with injected Hibernate DAO");
+        dbManager = da;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-   
-   public Ride createRide( String from, String to, Date date, int nPlaces, float price, String driverEmail ) throws RideMustBeLaterThanTodayException, RideAlreadyExistException{
-	   
-		dbManager.open();
-		Ride ride=dbManager.createRide(from, to, date, nPlaces, price, driverEmail);		
-		dbManager.close();
-		return ride;
-   };
-	
-   /**
-    * {@inheritDoc}
-    */
-	 
-	public List<Ride> getRides(String from, String to, Date date){
-		dbManager.open();
-		List<Ride>  rides=dbManager.getRides(from, to, date);
-		dbManager.close();
-		return rides;
-	}
+    @Override
+    public List<String> getDepartCities() {
+        return dbManager.getDepartCities();
+    }
 
-    
-	/**
-	 * {@inheritDoc}
-	 */
-	 
-	public List<Date> getThisMonthDatesWithRides(String from, String to, Date date){
-		dbManager.open();
-		List<Date>  dates=dbManager.getThisMonthDatesWithRides(from, to, date);
-		dbManager.close();
-		return dates;
-	}
-	
-	
-	public void close() {
-		DataAccess dB4oManager=new DataAccess();
+    @Override
+    public List<String> getDestinationCities(String from) {
+        return dbManager.getArrivalCities(from);
+    }
 
-		dB4oManager.close();
+    @Override
+    public Ride createRide(String from, String to, Date date, int nPlaces, float price, String driverEmail)
+            throws RideMustBeLaterThanTodayException, RideAlreadyExistException {
 
-	}
+        return dbManager.createRide(from, to, date, nPlaces, price, driverEmail);
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-    	
-	 public void initializeBD(){
-    	dbManager.open();
-		dbManager.initializeDB();
-		dbManager.close();
-	}
+    @Override
+    public List<Ride> getRides(String from, String to, Date date) {
+        return dbManager.getRides(from, to, date);
+    }
 
+    @Override
+    public List<Date> getThisMonthDatesWithRides(String from, String to, Date date) {
+        return dbManager.getThisMonthDatesWithRides(from, to, date);
+    }
+
+    @Override
+    public void initializeBD() {
+        dbManager.initializeDB();
+    }
+/*
+    @Override
+    public void close() {
+        // Si quieres podrías cerrar el SessionFactory:
+        // HibernateUtil.shutdown();
+    }*/
 }
-
--- src/main/java/eredua/bean/FacadeBean.java
+++ src/main/java/eredua/bean/FacadeBean.java
@@ -2,17 +2,22 @@ package eredua.bean;
 
 import businessLogic.BLFacade;
 import businessLogic.BLFacadeImplementation;
-import dataAccess.DataAccess;
+import dataAccess.HibernateDataAccess;
 
 public class FacadeBean {
-	private static FacadeBean singleton = new FacadeBean( ); 
-	private static BLFacade facadeInterface; 
-	private FacadeBean(){  
-		try { facadeInterface=new BLFacadeImplementation(new DataAccess()); }  
-		catch (Exception e) { 
-			System.out.println("FacadeBean: negozioaren logika sortzean errorea: "+e.getMessage()); 
-		}} 
-	public static BLFacade getBusinessLogic( ) {     
-		return facadeInterface; 
-	}
+
+    private static FacadeBean singleton = new FacadeBean();
+    private static BLFacade facadeInterface;
+
+    private FacadeBean() {
+        try {
+            facadeInterface = new BLFacadeImplementation(new HibernateDataAccess());
+        } catch (Exception e) {
+            System.out.println("FacadeBean: error creando la lógica de negocio: " + e.getMessage());
+        }
+    }
+
+    public static BLFacade getBusinessLogic() {
+        return facadeInterface;
+    }
 }
